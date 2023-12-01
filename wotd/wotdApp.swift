52d1c6310e4160553f6964b3a47ff848f6f72112
emilyj4482 <emilyj4482@gmail.com>
2023-12-01 15:50:49 +0900

    first commit

---
+++ wotd/wotdApp.swift
@@ -0,0 +1,17 @@
+//
+//  wotdApp.swift
+//  wotd
+//
+//  Created by EMILY on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct wotdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
