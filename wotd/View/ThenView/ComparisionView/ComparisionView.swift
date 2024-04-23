870416bb7e990fb41da34f43b791117fbc0f6cf1
emilyj4482 <emilyj4482@gmail.com>
2024-04-24 03:58:48 +0900

    비교 view에 2개의 rect 배치

--- wotd/View/ThenView/ComparisionView/ComparisionView.swift
+++ wotd/View/ThenView/ComparisionView/ComparisionView.swift
@@ -2,7 +2,7 @@
 //  ComparisionView.swift
 //  wotd
 //
-//  Created by EMILY on 06/04/2024.
+//  Created by EMILY on 23/04/2024.
 //
 
 import SwiftUI
@@ -12,56 +12,12 @@ struct ComparisionView: View {
     @State var weather: ThenWeather
     
     var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(.mint)
-                .frame(height: 250)
-                .clipShape(.rect(cornerRadius: 20))
+        VStack(spacing: 15) {
+            ComparisionRect(weather: $weather, isThen: true)
             
-            VStack {
-                /*
-                TempProgressView(weather: $weather)
-                    .padding(.bottom)
-                */
-                
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text(weather.date)
-                            .font(.title)
-                        Text(weather.city)
-                            .font(.title2).bold()
-                    }
-                    
-                    Spacer()
-                    
-                    VStack(alignment: .trailing, spacing: 10) {
-                        Text("9AM")
-                        Text("12PM")
-                        Text("6PM")
-                        Text("12AM")
-                    }
-                    .monospaced()
-                    .padding(.trailing, 10 )
-                    
-                    VStack(alignment: .trailing, spacing: 10) {
-                        Text("\(weather.morning)°")
-                        Text("\(weather.afternoon)°")
-                        Text("\(weather.evening)°")
-                        Text("\(weather.night)°")
-                    }
-                    .padding(.trailing, 16)
-                }
-                .padding(.horizontal)
-                
-                TempProgressView(weather: $weather)
-                    .padding(.top)
-                
-            }
+            ComparisionRect(weather: $weather, isThen: false)
         }
-        .padding(.horizontal, 20)
     }
-    
-    
 }
 
 #Preview {
