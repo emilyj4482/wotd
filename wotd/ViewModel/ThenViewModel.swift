cc4757200ad68ea19ffcb211a5027cb153afa5f3
emilyj4482 <emilyj4482@gmail.com>
2024-06-17 03:14:50 +0900

    온도 범위 수정

--- wotd/ViewModel/ThenViewModel.swift
+++ wotd/ViewModel/ThenViewModel.swift
@@ -56,7 +56,7 @@ extension ThenViewModel {
         let max = max(thenMax, nowMax)
         let min = min(thenMin, nowMin)
 
-        // 범위가 1...max가 되도록 조정
+        // 범위가 0...max가 되도록 조정
         let thenMinDouble = rerangeTemp(min: min, max: max, temp: thenMin)
         let thenMaxDouble = rerangeTemp(min: min, max: max, temp: thenMax)
         let nowMinDouble = rerangeTemp(min: min, max: max, temp: nowMin)
@@ -76,7 +76,7 @@ extension ThenViewModel {
         let lowerBound = min/rangeMax
         let upperBound = max/rangeMax
         
-        return lowerBound...upperBound/1
+        return lowerBound...upperBound
     }
     
     // 온도 범위 정제
@@ -84,11 +84,11 @@ extension ThenViewModel {
         var temp = temp
         
         if min < 0 {
-            temp = temp + abs(min) + 1
+            temp = temp + abs(min)
         } else if min == 0 {
-            temp += 1
+            
         } else {
-            temp = temp - abs(min) + 1
+            temp = temp - abs(min)
         }
         return Double(temp)
     }
