36bc20e061abf079937cabbe136efb63d1ae599a
emilyj4482 <emilyj4482@gmail.com>
2024-07-04 02:56:39 +0900

    setting view minor update

--- wotd/View/SettingView.swift
+++ wotd/View/SettingView.swift
@@ -8,16 +8,35 @@
 import SwiftUI
 
 struct SettingView: View {
+    
+    @State private var version: String = "1.0.0"
+    @State private var isLatest: Bool = true
+    
     var body: some View {
-        List {
-            Section("Info") {
-                Text("Version")
-                Text("Data source")
-            }
-            Section("Settings") {
-                Text("Display")
+        NavigationStack {
+            List {
+                Section("Info") {
+                    VStack(alignment: .leading) {
+                        Text("Version")
+                        Text("\(version) \(isLatest ? "is up to date" : "needs update")")
+                            .font(.caption)
+                            .foregroundStyle(.primary.opacity(0.5))
+                    }
+                    NavigationLink("Data source") {
+                        DataSourceView()
+                    }
+                }
+                Section("Settings") {
+                    Text("Display")
+                }
             }
         }
+    }
+}
+
+private struct DataSourceView: View {
+    fileprivate var body: some View {
+        Text("OpenWeather API")
     }
 }
 
